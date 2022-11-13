7035b8b306f38a580f76928b9c969af090444a0f
YehiaMousaX <118112478+YehiaMousaX@users.noreply.github.com>
2022-11-13 23:52:29 +0100

    Update Course.js

--- src/Models/Course.js
+++ src/Models/Course.js
@@ -2,45 +2,50 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const CourseSchema = new Schema({
-    Courseid : {
+    Courseid: {
         type: String,
         required: true,
         unique: true
     },
-   
+
+    Subject: {
+        type: String,
+        required: true
+    },
+
     title: {
         type: String,
         required: true,
-        
+
     },
     subtitle: {
         type: String,
         required: true,
-        
+
     },
     price: {
         type: Number,
         required: true,
-        
+
     },
     shortsummary: {
         type: String,
         required: true,
-        
-    },
-    rating:{
-        type: Number,
-        required: false ,
 
-        
+    },
+    rating: {
+        type: Number,
+        required: false,
+
+
     },
     review: {
         type: String,
-        required: false ,
-       
+        required: false,
+
     }
 
-   
+
 }, { timestamps: true });
 
 

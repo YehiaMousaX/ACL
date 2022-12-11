1b92b7dffd4b780a5f4b26bb41a970d1b5c62b26
Ahmed-Elkallawy <ahmedal3awy@gmail.com>
2022-12-11 20:27:49 +0200

    Update userController.js

--- BackEnd/src/Routes/userController.js
+++ BackEnd/src/Routes/userController.js
@@ -259,6 +259,12 @@ const  countryList = [
 const X =[] ;
 
 
+const y =[] ;
+router.get("/search", async(req, res) => {
+    y.push ( await Course.find({}, {title : req.body.title}));
+    
+    res.send(y);
+});
 
 router.get("/rate/:rating", async(req, res) => {
     const Courses = await Course.find({ rating: req.params['rating'] });

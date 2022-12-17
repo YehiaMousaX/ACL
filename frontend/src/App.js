e8bcca0f300db30e4b1e11d58b4c4dc3fdee15f3
YehiaMousaX <118112478+YehiaMousaX@users.noreply.github.com>
2022-12-17 04:06:53 +0100

    yehia

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,17 +1,9 @@
-//import SignUpForm from './components/SignUp/signup';
-//import CourseOutline from './components/courseOutline';
-//import LandingPage from './components/landingPage';
-import Navbar from "./components/navbar";
-//import MultipleChoiceExercise from './components/multipleChoiceExercise';
-//import ProfilePage from "./components/profileInstructor";
-//import CoursesList from "./components/courseList";
-//import AddCourseForm from "./components/addCourse";
-import AdminAdd from "./components/adminAdd";
+
+import SignUpForm from './components/SignUp/signup';
+import Header from './components/SignUp/header';
 const App = () => {
   return (
     <div>
-      <Navbar/>
-      <AdminAdd/>
       
     </div>
   );

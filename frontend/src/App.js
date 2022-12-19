b0d4a21863cbf7278788a022fcf0f65484ca199c
YehiaMousaX <118112478+YehiaMousaX@users.noreply.github.com>
2022-12-19 19:29:01 +0100

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,13 @@
 
 //import SignUpForm from './components/SignUp/signup';
 import Navbar from './components/navbar';
+import LandingPage from './components/landingPage';
 
 const App = () => {
   return (
     <div>
       <Navbar/>
+      <LandingPage/>
     </div>
   );
 };

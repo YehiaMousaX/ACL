3554a85926695f03760477bd3cd800897764645e
seifahmed74 <95249784+seifahmed74@users.noreply.github.com>
2022-12-21 23:57:56 +0100

    seif

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,4 @@
-import UseropenregisteredCourses from "./components/UseropenExamCourses";
-import Addquestion from "./components/Addquestion";
 import UserRateCourse from "./components/userratecourse";
-import UserRateInstructor from "./components/userrateinstractor";
 const App = () => {
   return (
     <div>

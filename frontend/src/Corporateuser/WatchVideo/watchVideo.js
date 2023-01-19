5f4b62ff51b6fab32df1da504f126efc8e3bc601
MDawod8 <midodawod7551@gmail.com>
2023-01-19 13:23:09 +0200

    Update watchVideo.js

--- frontend/src/Corporateuser/WatchVideo/watchVideo.js
+++ frontend/src/Corporateuser/WatchVideo/watchVideo.js
@@ -1,7 +1,5 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
-import { useHistory } from 'react-router-dom'
-const history = useHistory();
 
 function CorporateUserVideoPlayer({ match }) {
     const [videoUrl, setVideoUrl] = useState(null);
